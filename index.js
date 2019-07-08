8f887aec0f948958a25bdf15dc57064032fe9bcf
Elijah <efloren@siue.edu>
2019-07-08 15:50:35 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time)
+{
+  debugger
+  if (time.split(':')[0] < 12) { return "Good Morning" }
+  else if ((time.split(':')[0] < 17)&&(time.split(':')[0] > 12)) { return "Good Afternoon"}
+  else { return "Good Evening"}
+
+}
+
+function displayMessage(message)
+{
+  var element;
+  element = document.querySelector('h1#greeting');
+  element.textContent = message;
+  
+}
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
