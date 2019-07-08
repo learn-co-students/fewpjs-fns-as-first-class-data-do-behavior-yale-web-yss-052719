be04d903ad293f49abd6c22ef9558120d007e395
rinakawamura <rina.kawamura@yale.edu>
2019-07-08 14:25:54 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = Number(time.split(':')[0]);
+  if (hour < 12) {
+      return "Good Morning";
+  } else if (hour > 17) {
+      return "Good Evening";
+  } else {
+      return "Good Afternoon";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  greeting = document.getElementById("greeting");
+  greeting.innerText = msg;
+}
