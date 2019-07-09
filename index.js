54bb6488f9eec53fb6f66e5383f1de38855c6e06
maxsunnyday <max.sun@yale.edu>
2019-07-09 09:34:38 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = time.split(":")[0]
+  const hour_int = parseInt(hour, 10)
+  if (hour_int < 12) {
+    return "Good Morning"
+  } 
+  
+  if (hour_int > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg
+}
